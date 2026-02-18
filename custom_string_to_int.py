730d895783343c9adf6c5e4e531c49dec3e3f4fc
lelooouch <118593450+lelooouch@users.noreply.github.com>
2026-02-18 11:59:46 +0500

    Update custom_string_to_int.py

--- custom_string_to_int.py
+++ custom_string_to_int.py
@@ -1,5 +1,5 @@
-list1 = [1,2,3,4,5,6,7,8,9]
-list2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+list1 = [0, 1,2,3,4,5,6,7,8,9]
+list2 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def custom_string_to_int(number_str):
 
@@ -8,26 +8,12 @@ def custom_string_to_int(number_str):
 
     number_int = 0
     for i in range(len(number_str)):
-        if number_str[i] == '1':
-            number_int += 1 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] == '2':
-            number_int += 2 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] == '3':
-            number_int += 3 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] == '4':
-            number_int += 4 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] == '5':
-            number_int += 5 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] == '6':
-            number_int += 6 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] == '7':
-            number_int += 7 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] == '8':
-            number_int += 8 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] == '9':
-            number_int += 9 * 10 ** (len(number_str) - i - 1)
-        elif number_str[i] != '0' and (number_str[i] != '-' and i != 1):
-            raise ValueError('Ошибка')
+        for j in range(10):
+            if number_str[i] not in '0123456789-':
+                raise ValueError('Ошибка')
+            elif number_str[i] == list2[j]:
+                number_int += list1[j] * 10 ** (len(number_str) - i - 1)
+                break
 
     if number_str[0] == '-':
         number_int = -1 * number_int
